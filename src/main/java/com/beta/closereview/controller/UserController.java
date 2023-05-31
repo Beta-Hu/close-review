8ae46ea3dd214076a51877c11a82ec2ce0ef787d
Beta <betahu@foxmail.com>
2023-05-31 17:21:40 +0800

    aaa

--- src/main/java/com/beta/closereview/controller/UserController.java
+++ src/main/java/com/beta/closereview/controller/UserController.java
@@ -3,15 +3,14 @@ package com.beta.closereview.controller;
 import com.beta.closereview.dao.User;
 import com.beta.closereview.enums.StatusEnum;
 import com.beta.closereview.service.UserService;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
 
