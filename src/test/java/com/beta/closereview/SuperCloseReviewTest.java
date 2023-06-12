44f1330c94ee0e94cdbc60d1c5db3628b3efe890
BetaHu <419859260@qq.com>
2023-06-12 20:19:16 +0800

    含有开发文档的版本

--- src/test/java/com/beta/closereview/SuperCloseReviewTest.java
+++ src/test/java/com/beta/closereview/SuperCloseReviewTest.java
@@ -1,6 +1,5 @@
 package com.beta.closereview;
 
-import com.beta.closereview.dao.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
